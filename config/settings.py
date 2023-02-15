237b141dfe37f960c1ea12ab0507173fcff33c89
3o14 <dldnjswn4265@gmail.com>
2023-02-15 15:25:05 +0900

    -

--- config/settings.py
+++ config/settings.py
@@ -11,13 +11,13 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os 
 from django.core.exceptions import ImproperlyConfigured
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
