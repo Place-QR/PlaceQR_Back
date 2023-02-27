3e89a60de5eab0ec0aadc6c5e647615a5f0a0ce2
JHeaon <j3haewon@gmail.com>
2023-02-27 13:25:20 +0900

    delete photos models

--- photos/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from .models import Photo
-
-class PhotoSerializer(ModelSerializer):
-    class Meta:
-        model = Photo
-        fields = "__all__"
